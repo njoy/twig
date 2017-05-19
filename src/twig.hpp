ed81856a7afadd53c4ce82cf8f1227ea68128569
Austin McCartney <amccartney@lanl.gov>
2017-05-19 16:15:42 -0600

    initial commit

---
+++ src/twig.hpp
@@ -0,0 +1,16 @@
+#ifdef NJOY21_TWIG
+#define NJOY21_TWIG
+
+#include <algorithms>
+#include <vector>
+
+namespace njoy {
+namespace twig {
+
+#include "twig/UnionGrid.hpp"
+#include "twig/Linearization.hpp"
+
+}
+}
+
+#endif
